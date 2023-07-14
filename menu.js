276351067fa4d2e1dfeeaebbccf971463ed5fecf
Nadika <nadika.name@gmail.com>
2023-07-14 14:20:02 +0100

    init: commit all files

---
+++ menu.js
@@ -0,0 +1,7 @@
+// function checkMediaQuery() {
+//   if (window.innerWidth > 570) {
+//     document.getElementById("icon_menu").src = "images/icons/menu_sm_screen.png"
+//   }
+// }
+
+// window.addEventListener('resize', checkMediaQuery);
